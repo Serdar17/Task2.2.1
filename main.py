8544118a084ba02bd11bc8037877e0c72d4f72cf
Serdar Ablyametov <useinovserdar23@gmail.com>
2022-11-23 11:30:04 +0500

    Changed input in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Task5_2 import print_vacancies
 from Task2_1_3 import main_pdf
 
-data = input("Введите 'Вакансии' для формирование табличнх вакансии или 'Статистика' для формирования отчета: ")
+data = input("Введите 'Вакансии' для вывода табличнх вакансии или 'Статистика' для pdf отчета: ")
 if data == "Вакансии":
     print_vacancies()
 elif data == "Статистика":
